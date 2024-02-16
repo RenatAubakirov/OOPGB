844160a5a90c59914698b7ea06ffd91911a7187d
Renat Aubakirov <arenat98@mail.ru>
2024-02-16 10:55:17 +0700

    1

--- Seminar1/VendingMachine/src/App.java
+++ Seminar1/VendingMachine/src/App.java
@@ -1,5 +1,10 @@
+import domen.Product;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Product product = new Product(
+            1, "lays", 132.55 , 12
+        );
+        System.out.println(product);
     }
 }
