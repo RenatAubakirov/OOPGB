b4d4dd42ccdee1b930cc944324cdf27a9567959e
Renat Aubakirov <arenat98@mail.ru>
2024-02-21 12:47:27 +0700

    promoClient

--- Seminar2/SuperMarketApp/src/Main.java
+++ Seminar2/SuperMarketApp/src/Main.java
@@ -1,6 +1,7 @@
 import classes.Actor;
 import classes.Market;
 import classes.OrdinaryClient;
+import classes.PromoClient;
 import classes.SpecialClient;
 import classes.TaxInspector;
 
@@ -11,12 +12,14 @@ public class Main {
         Actor client2 = new OrdinaryClient("Оля");
         Actor client3 = new SpecialClient("Президент", 777);
         TaxInspector client4 = new TaxInspector();
+        Actor client5 = new PromoClient("Саша", 21, "2 по цене 3", 5);
         
 
         magnit.acceptToMarket(client1);
         magnit.acceptToMarket(client2);
         magnit.acceptToMarket(client3);
         magnit.acceptToMarket(client4);
+        magnit.acceptToMarket(client5);
 
         magnit.update();
     }
---
+++ Seminar2/SuperMarketApp/src/classes/PromoClient.java
@@ -0,0 +1,57 @@
+package classes;
+/**
+ * Класс, представляющий акционного клиента.
+ * Унаследован от класса Actor.
+ */
+public class PromoClient extends Actor {
+    // Поля класса
+    private int idPromo; // Идентификатор акции
+    private String promoName; // Название акции
+    private static int participantsCount; // Количество участников акции (поле статическое)
+
+    /**
+     * Конструктор класса PromoClient.
+     *
+     * @param name            Имя акционного клиента.
+     * @param idPromo         Идентификатор акции.
+     * @param promoName       Название акции.
+     * @param participantsCount Количество участников акции.
+     */
+    public PromoClient(String name, int idPromo, String promoName, int participantsCount) {
+        super(name); // Вызов конструктора суперкласса для установки имени клиента
+        this.idPromo = idPromo;
+        this.promoName = promoName;
+        PromoClient.participantsCount = participantsCount; // Установка количества участников акции
+    }
+
+
+    public boolean isMakeOrder() {
+        return super.isMakeOrder;
+    }
+
+    public boolean isTakeOrder() {
+        return super.isTakeOrder;
+    }
+
+    public void setMakeOrder(boolean isCreated) {
+        super.isMakeOrder = isCreated;
+    }
+
+    public void setTakeOrder(boolean isCreated) {
+        super.isTakeOrder = isCreated;
+    }
+
+    public Actor getActor() {
+        return this;
+    }
+
+    @Override
+    String getName() {
+        return super.name;
+    }
+
+    @Override
+    void setName(String name) {
+        super.name = name;
+    }
+}
