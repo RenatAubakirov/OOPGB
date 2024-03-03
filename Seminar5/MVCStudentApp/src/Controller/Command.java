97e98dd68eba4348605443cbfb19174a43453308
Renat Aubakirov <arenat98@mail.ru>
2024-03-03 07:34:48 +0700

    first

---
+++ Seminar5/MVCStudentApp/src/Controller/Command.java
@@ -0,0 +1,12 @@
+package Controller;
+
+public enum Command {
+    NONE,
+    // FIXME реализовать read и create в будущих релизах
+    // READ,
+    // CREATE,
+    LIST,
+    EXIT,
+    DELETE
+}
+
